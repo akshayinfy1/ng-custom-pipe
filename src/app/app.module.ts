90c73e25dddf14f5696c8fce325574074316e6bd
Akshay <akshaywhizit@gmail.com>
2023-08-20 21:46:42 +0530

    ng-custom-pipe demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgCustomPipeModule } from 'ng-custom-pipe';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCustomPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
